cf2e18a04a5c7db3164a0ccb17dc4a602fd798c6
Stefano Locati <stivlo@gmail.com>
2019-06-03 10:18:36 +0100

    moved forecast in a separate module

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,14 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// weather
-// const weatherKey = '820c6ff6235d3223e159c1be2c694253';
-// const lat = 51.545523;
-// const long = -0.126658;
-// const weatherUrl = `https://api.darksky.net/forecast/${weatherKey}/${lat},${long}?units=si&lang=it`;
-// request({ url: weatherUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service');
-//     } else if (response.statusCode / 100 != 2) {
-//         console.log('Unable to find location');
-//     } else {
-//         const currently = response.body.currently;
-//         console.log(response.body.daily.data[0].summary +
-//             ` It is currently ${currently.temperature} degrees out. ` +
-//             `There is a ${currently.precipProbability}% chance of rain.`);
-//     }
+
+// geocode('97 York Way London', (error, data) => {
+//     console.log('error:', error);
+//     console.log('data:', data);
 // });
 
 
-geocode('97 York Way London', (error, data) => {
-    console.log('error:', error);
-    console.log('data:', data);
+forecast(44.1545, -75.7088, (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 });
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+    const weatherKey = '820c6ff6235d3223e159c1be2c694253';
+    const weatherUrl = `https://api.darksky.net/forecast/${weatherKey}/${lat},${long}?units=si&lang=en`;
+    request({ url: weatherUrl, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service');
+        } else if (response.statusCode / 100 != 2) {
+            callback('Unable to find location');
+        } else {
+            const currently = response.body.currently;
+            callback(undefined, response.body.daily.data[0].summary +
+                ` It is currently ${currently.temperature} degrees out. ` +
+                `There is a ${currently.precipProbability}% chance of rain.`);
+        }
+    });
+};
+
+module.exports = forecast;
