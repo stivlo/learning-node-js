e6a320b67254599f5cb5b9e1dcb0d019c55a8c09
Stefano Locati <stivlo@gmail.com>
2019-06-04 20:42:27 +0100

    experimenting with nodejs and express

---
+++ web-server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'London',
+        forecast: 'Good weather'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
