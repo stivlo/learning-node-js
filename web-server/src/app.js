e85d82c05308dba1a6148f86a80cd38278e2b84b
Stefano Locati <stivlo@gmail.com>
2019-06-05 11:55:28 +0100

    added mustache templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Helpful message'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
