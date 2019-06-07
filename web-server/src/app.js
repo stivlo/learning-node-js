dbd13f3b00a9be0c7abe5c991d051c86c9df769a
Stefano Locati <stivlo@gmail.com>
2019-06-07 21:11:46 +0100

    Added 404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,9 +4,9 @@ const hbs = require('hbs');
 const app = express();
 
 // set up handlebars engine and views location
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 app.set('views', path.join(__dirname, '../templates/views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -36,6 +36,20 @@ app.get('/weather', (req, res) => {
     res.send({
         location: 'London',
         forecast: 'Good weather'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Help article Not Found',
+        name: 'Stefano Locati'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Page Not Found',
+        name: 'Stefano Locati'
     });
 });
 
