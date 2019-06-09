2c5617caa6b2478253eaf9978123acf8cc9eab57
Stefano Locati <stivlo@gmail.com>
2019-06-09 13:16:58 +0100

    Created search form for weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,32 @@
-console.log('Client side JavaScript file loaded.');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+function init() {
+    const location = localStorage.getItem('location');
+    if (location) {
+        search.value = location;
+        updateWeather(location);
+    }
+}
+
+weatherForm.addEventListener('submit', (event) => {
+    const location = search.value;
+    localStorage.setItem('location', location);
+    updateWeather(location);
+    event.preventDefault();
+});
+
+function updateWeather(location) {
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
+    });
+}
+
+init();
