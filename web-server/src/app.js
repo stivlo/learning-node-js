fddf14521ccd316fc98ca09fac25f982be5e578b
Stefano Locati <stivlo@gmail.com>
2019-06-09 11:41:10 +0100

    added default object to deal with the case when an error is thrown

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -41,7 +41,7 @@ app.get('/weather', (req, res) => {
             error: 'address must be provided'
         });
     }
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return res.send({ error });
         }
