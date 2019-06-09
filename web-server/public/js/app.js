8ffa7decfa99e07c31011dc0b6512b09bac4dfcc
Stefano Locati <stivlo@gmail.com>
2019-06-09 13:39:37 +0100

    weather app completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -17,13 +17,18 @@ weatherForm.addEventListener('submit', (event) => {
 });
 
 function updateWeather(location) {
+    const messageOne = document.querySelector('#message-1');
+    const messageTwo = document.querySelector('#message-2');
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;
+                messageTwo.textContent = '';
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             }
         });
     });
